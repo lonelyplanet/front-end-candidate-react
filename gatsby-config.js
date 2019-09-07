055857036ae8554c97256ef6faa558c226e27f09
vacas5 <russelljanderson@gmail.com>
2019-09-07 09:32:22 -0500

    instructions for readme and some metadata changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lonely Planet`,
+    description: `a limited code challenge testing the ability to render and style data using React and graphql`,
+    author: `@lonelyplanet`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
